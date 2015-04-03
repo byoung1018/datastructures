0ae2adf80abb0588e31569cf4a7edce6d46883c1
Bryce <byoung1018@gmail.com>
2015-04-02 17:08:11 -0700

    finish bst specs

--- specs/bst_spec.rb
+++ specs/bst_spec.rb
@@ -42,4 +42,83 @@ describe BST do
     end
   end
 
+  describe "#contains" do
+    it "can return whether the tree has the given value or not" do
+      bfs_compare([8, 3, 10, 1, 6, 14, 4, 7, 13])
+      expect(bst.contains(8)).to be_truthy
+      expect(bst.contains(3)).to be_truthy
+      expect(bst.contains(10)).to be_truthy
+      expect(bst.contains(1)).to be_truthy
+      expect(bst.contains(6)).to be_truthy
+      expect(bst.contains(14)).to be_truthy
+      expect(bst.contains(4)).to be_truthy
+      expect(bst.contains(7)).to be_truthy
+      expect(bst.contains(13)).to be_truthy
+      expect(bst.contains(2)).to be_falsy
+      expect(bst.contains(50)).to be_falsy
+      expect(bst.contains(-1)).to be_falsy
+    end
+
+    it "does not mutate the tree" do
+      bfs_compare([8, 3, 10, 1, 6, 14, 4, 7, 13])
+    end
+  end
+
+  describe "#each_descending" do
+    it "" do
+      descending = []
+      bst.each_descending {|node| descending << node.value}
+      expect(descending).to eq([14, 13, 10, 8, 7, 6, 4, 3, 1])
+    end
+  end
+
+  describe "#each_ascending" do
+    it "" do
+      ascending = []
+      bst.each_ascending {|node| ascending << node.value}
+      expect(ascending).to eq([1, 3, 4, 6, 7, 8, 10, 13, 14])
+    end
+  end
+
+  describe "#remove" do
+    it "can remove a leaf" do
+      bst.remove(4)
+      bfs_compare([8, 3, 10, 1, 6, 14, 7, 13])
+    end
+    it "can remove a node with one child" do
+      bst.remove(14)
+      bfs_compare([8, 3, 10, 1, 6, 13, 4, 7])
+    end
+    it "can remove a node with two children" do
+      bst.remove(3)
+      bfs_compare([8, 4, 10, 1, 6, 14, 7, 13])
+    end
+
+    it "can remove the root with one child" do
+      bst.remove(3)
+      bst.remove(6)
+      bst.remove(1)
+      bst.remove(4)
+      bst.remove(7)
+      bst.remove(8)
+      bfs_compare([10, 14, 13])
+    end
+
+    it "can remove the root with two children" do
+      bst.remove(8)
+      bfs_compare([10, 3, 14, 1, 6, 13, 4, 7, 13])
+    end
+  end
+
+  describe "#highest" do
+    it "returns the highest number" do
+      expect(bst.highest).to eq(14)
+    end
+  end
+
+  describe "#lowest" do
+    it "returns the lowest number" do
+      expect(bst.lowest).to eq(1)
+    end
+  end
 end
