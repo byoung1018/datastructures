f4303aae602a0be764f1d38d00293030e7f9b542
Bryce <byoung1018@gmail.com>
2015-04-02 17:01:08 -0700

    fill first specs for linked list

--- lib/lists/linked_list.rb
+++ lib/lists/linked_list.rb
@@ -1,5 +1,22 @@
 require_relative "list_node"
 
 class LinkedList
+  attr_reader :count
+  def initialize()
+    @head = nil
+    @tail = nil
+    @count = 0
+  end
 
+  def add(value)
+    if @tail
+      @tail.next = Node.new(value)
+      @tail = @tail.next
+    else
+      @head = Node.new(value)
+      @tail = @head
+    end
+    @count += 1
+  end
+  
 end
